bfb692e6e5512298717840ea1736dad1d1236462
AkagiYutaro <90638538+AkagiYutaro@users.noreply.github.com>
2026-08-01 16:02:27 +0900

    Add files via upload

--- logic/pick_logic.py
+++ logic/pick_logic.py
@@ -108,7 +108,7 @@ class PickPhaseManager:
         embed.add_field(name="\u200B", value=f"** 🚫 BAN**\n**🔵 チームA**\n{ban_a_str}", inline=True)
         embed.add_field(name="\u200B", value=f"\u200B\n**🔴 チームB**\n{ban_b_str}", inline=True)
         
-        embed.add_field(name="\u200B", value="\u200B", inline=False)
+        embed.add_field(name="\u200B", value="\u200B", inline=True)
         
         now_jst = datetime.datetime.now(JST)
         timestamp = now_jst.strftime("%Y/%m/%d %H:%M:%S")
