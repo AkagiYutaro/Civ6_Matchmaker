14f46694021b9b4e752be0ed796a16abf8aa0252
AkagiYutaro <90638538+AkagiYutaro@users.noreply.github.com>
2026-08-01 14:25:29 +0900

    Add files via upload

--- logic/pick_logic.py
+++ logic/pick_logic.py
@@ -105,7 +105,7 @@ class PickPhaseManager:
         # 2. チームBAN
         ban_a_str = format_leader_list(self.banned_a, self.all_leaders)
         ban_b_str = format_leader_list(self.banned_b, self.all_leaders)
-        embed.add_field(name="\u200B", value=f"** 🚫 BAN**\n**🔵 チームA**\n{ban_a_str}", inline=False)
+        embed.add_field(name="\u200B", value=f"** 🚫 BAN**\n**🔵 チームA**\n{ban_a_str}", inline=True)
         embed.add_field(name="\u200B", value=f"\u200B\n**🔴 チームB**\n{ban_b_str}", inline=True)
         
         # embed.add_field(name="\u200B", value="\u200B", inline=False)
@@ -137,7 +137,9 @@ class PickPhaseManager:
         pick_b_str = get_pick_str(self.team_b, "チームB")
         
         # 3. PICK
-        embed.add_field(name="\u200B", value=f"** ✅ PICK**\n**🔵 チームA**\n{pick_a_str}", inline=False)
+        embed.add_field(name="\u200B", value=f"** ✅ PICK**", inline=False)
+        embed.add_field(name="\u200B", value=f"** 🔵 チームA**\n{pick_a_str}", inline=True)
+        # embed.add_field(name="\u200B", value=f"** ✅ PICK**\n**🔵 チームA**\n{pick_a_str}", inline=True)
         embed.add_field(name="\u200B", value=f" \u200B\n**🔴 チームB**\n{pick_b_str}", inline=True)
         # embed.add_field(name="\u200B", value="\u200B", inline=False)
         
